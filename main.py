97f2ae4e1849235ea29974cad5846d8d1e36e0da
huijkman <johan@q42.nl>
2013-09-18 22:36:39 +0200

    small change to main.py just to see the effect :-)

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ import jinja2
 
 password = 'wachtwoord'
 
-
 jinja_environment = jinja2.Environment(autoescape=True,
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'views')))
 
